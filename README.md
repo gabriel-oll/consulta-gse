f35878b69316af930074882964c8a3609a51b0d0
gabriel-oll <gabb.webdev@gmail.com>
2022-05-26 20:50:44 -0300

    finishing consulta-gse_v1

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # consulta-gse
 
---
-
 ## Objetivo do projeto:
 
-### O objetivo do projeto em si é fazer um sistema que ira pegar    
+### O objetivo do projeto em si é fazer um sistema que irá pesquisar o endereço de IP e irá fazer uma pesquisa na API do Google: GOOGLE CUSTOM SEARCH API.
+
+irei destacar todos os campos em que você deve inserir uma API KEY, para obte-la você deve acessar o site da google cloud platform e se registrar
+
+ [link do GCP](https://console.cloud.google.com/)  
+
+Após realizar o registro crie um projeto, ele ficará vazio e você deve adicionar uma API para funcionar, a interface é intuitiva e no canto esquerdo tem uma opção chamada **APIs e serciços** e dentro dele **biblioteca**, clique nela e será direcionado para as API's disponíveis, selecione duas, a **Maps javascript API** e a **Custom Search API**, ative-as e elas gerarão uma chave de API, guarde pois é um dado sensível.
+
+Siga esses passos e alguns itens entrarão no caminho, primeiro, a API do maps solicitará que você crie uma conta de faturamento, **ALGUMAS OBSERVAÇÕES:** O Google disponibilizará U$300,00 de crédito para que você possa utilizar essa API sem custos, contudo ela é paga e você deve ser sábio em seus testes, pois após a API tem limite de requisições diárias e expira após o período de 90 dias, então veja com sabedoria e não dê refresh em vão, caso contrário só poderá utilizar a API depois de 24 horas.
+
+Após esses preenchimentos você estará apto a ativar as APIS e utilizá-las, crie as credenciais e as insira nos campos destacados da seguinte forma:
+
+**[YOUR GSE API KEY] = Custom search API = app.js(33)**
+
+**[YOUR GSE CX] = Custom search credential = app.js(33)**
+
+**[YOUR GMAPS API KEY] = Maps javascript API = index.html(103)**
+
+### Espero que dê tudo certo e qualuqer bug entre em contato: **gabb.webdev@gmail.com**
--- assets/app/app.js
+++ assets/app/app.js
@@ -1,38 +1,78 @@
-var ipValue = $(".ip-input");
+$(function () {
+  var ipValue = $(".ip-input");
 
-$(".ip-btn").click(function (a) {
-a.preventDefault();
+  $(".ip-btn").click(function (a) {
+    a.preventDefault();
 
-ip = ipValue.val();
+    ip = ipValue.val();
 
-var url = "http://ip-api.com/json/" + ip + "?lang=pt-BR";
+    var url = "http://ip-api.com/json/" + ip + "?lang=pt-BR";
 
-$.ajax({
-    method: "POST",
-    url: url,
-    data: ip,
-    dataType: "json"
-  }).done(function(data){
-      var countryCode = data['countryCode'];
-      var country = data['country'];
-      var city = data['city'];
-      var regionName = data['regionName'];
+    $.ajax({
+      method: "POST",
+      url: url,
+      data: ip,
+      dataType: "json",
+    }).done(function (data) {
+      var countryCode = data["countryCode"];
+      var country = data["country"];
+      var city = data["city"];
+      var regionName = data["regionName"];
+      var lat = data["lat"];
+      var lon = data["lon"];
+      var position = { lat: lat, lng: lon };
 
-      $('#countryCode').html(countryCode);
-      $('#country').html(country);
-      $('#city').html(city);
-      $('#regionName').html(regionName);
-      
-      var gseUrl = "https://www.googleapis.com/customsearch/v1?searchType=image&key=AIzaSyCGvkyavlUGQJN8ognVNM0wDRoERpR7yok&cx=c05e46acb65f8482e&q=" + country + " flag";
+      initMap(position);
+
+      $("#countryCode").html(countryCode);
+      $("#country").html(country);
+      $("#city").html(city);
+      $("#regionName").html(regionName);
+
+      var gseUrl =
+        "https://www.googleapis.com/customsearch/v1?searchType=image&key=[YOUR GSE API KEY]&cx=[YOUR GSE CX]&q=" +
+        country +
+        " flag";
       $.ajax({
         method: "GET",
         url: gseUrl,
         data: regionName,
-        dataType: "json"
-      }).done(function(data){
-          var imgResponse = data['items'][0]['link'];
+        dataType: "json",
+      }).done(function (data) {
+        console.log(data);
+        var imgResponse = data["items"][0]["link"];
+        $("#flagImg").attr("src", imgResponse);
 
-          $('#flagImg').attr('src', imgResponse);
+        var imgHeight = $("#flagImg").height();
+        console.log(imgHeight);
+
+        if (imgHeight === imgHeight) {
+          console.log("é igual");
+          var flagStyle2 = $("#map").height();
+          console.log(flagStyle2);
+        } else {
+          
+          var flagStyle = $("#flagImg").height();
+          console.log(flagStyle);
+          console.log("n é igual");
+        }
       });
+    });
   });
 });
+let map;
+
+function initMap(position) {
+
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: position,
+    zoom: 15,
+  });
+
+  const marker = new google.maps.Marker({
+    position: position,
+    map: map,
+  });
+
+}
+
